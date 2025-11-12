b229f296f616ca5cb7c049922f0e20dbf0028b71
Polina Trevogina <polina.trevogina@gmail.com>
2025-11-12 14:23:54 +0300

    Initial commit

---
+++ src/test/java/TestBase.java
@@ -0,0 +1,25 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class TestBase {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.baseUrl = "https://github.com";
+        Configuration.browserSize = "1920x1080";
+
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--ignore-ssl-errors=yes");
+        options.addArguments("--ignore-certificate-errors");
+
+
+        // Configuration.selenideHttpServerEnabled = false;
+
+//        Configuration.browser = "chrome";
+//        Configuration.holdBrowserOpen = true;
+    }
+}
